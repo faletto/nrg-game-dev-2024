e167a9860627f8bd4149ce6917b36af8f8e925e6
GuestUser1000101 <ryanzou2008@gmail.com>
2024-07-09 19:26:28 -0700

    Add unsaved code from previous commit

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -6,8 +6,9 @@ public class Bird : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
     [SerializeField] private float flapStrength;
-    public bool IsGameOver = false;
     [SerializeField] private GameObject deathScreen;
+
+    public bool IsGameOver = false;
 
     // Start is called before the first frame update
     void Start()
